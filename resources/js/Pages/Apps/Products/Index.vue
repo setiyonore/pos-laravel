e7f27ee12a226268af4c7b9065b8ec86aedfe3fb
stynore <setiyono.ressly@gmail.com>
2022-07-25 18:38:18 +0700

    add destroy and show barcode

--- resources/js/Pages/Apps/Products/Index.vue
+++ resources/js/Pages/Apps/Products/Index.vue
@@ -33,14 +33,19 @@
                                     </thead>
                                     <tbody>
                                         <tr v-for="(product,index) in products.data" :key="index">
-                                            <td class="text-center">{{ product.barcode }}</td>
+                                            <barcode
+                                                :value="product.barcode"
+                                                :format="'CODE39'"
+                                                :lineColor="'#000'"
+                                                :width="1"
+                                                :height="20" />
                                             <td>{{ product.title }}</td>
                                             <td>Rp. {{ formatPrice(product.buy_price) }}</td>
                                             <td>Rp. {{ formatPrice(product.sell_price) }}</td>
                                             <td>{{ product.stock }}</td>
                                             <td class="text-center">
                                                 <Link :href="`/apps/products/${product.id}/edit`" v-if="hasAnyPermission(['products.edit'])" class="btn btn-success btn-sm me-2"><i class="fa fa-pencil-alt me-1"></i> EDIT</Link>
-                                                <button v-if="hasAnyPermission(['products.delete'])" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i> DELETE</button>
+                                                <button @click.prevent="destroy(product.id)" v-if="hasAnyPermission(['products.delete'])" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i> DELETE</button>
                                             </td>
                                         </tr>
                                     </tbody>
@@ -66,6 +71,8 @@ import Pagination from '../../../Components/Pagination';
 import {Head,Link} from '@inertiajs/inertia-vue3';
 import {ref} from 'vue';
 import {Inertia} from '@inertiajs/inertia';
+import Swal from 'sweetalert2';
+import Barcode from '../../../Components/Barcode';
 
 export default {
     layout: LayoutApp,
@@ -73,6 +80,7 @@ export default {
         Head,
         Link,
         Pagination,
+        Barcode,
     },
     props: {
         products: Object,
@@ -84,10 +92,35 @@ export default {
                 q: search.value,
             });
         }
+        const destroy = (id) => {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            })
+                .then((result) => {
+                    if (result.isConfirmed) {
+                        Inertia.delete(`/apps/products/${id}`);
+
+                        Swal.fire({
+                            title: 'Deleted!',
+                            text: 'Product deleted successfully.',
+                            icon: 'success',
+                            timer: 2000,
+                            showConfirmButton: false,
+                        });
+                    }
+                })
+        }
 
         return {
             search,
             handleSearch,
+            destroy,
         }
     }
 }
