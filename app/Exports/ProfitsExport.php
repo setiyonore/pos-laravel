ed2135a6db38104cbca459458c4d898919d4f04d
stynore <setiyono.ressly@gmail.com>
2022-07-27 21:18:03 +0700

    fix export excell profits

--- app/Exports/ProfitsExport.php
+++ app/Exports/ProfitsExport.php
@@ -17,11 +17,11 @@ class ProfitsExport implements FromView
         $this->start_date = $start_date;
         $this->end_date = $end_date;
     }
-    public function view(): view
+    public function view(): View
     {
         return view('exports.profits',[
             'profits' => Profit::with('transaction')->whereDate('created_at','>=',$this->start_date)->whereDate('created_at','<=',$this->end_date)->get(),
-            'total' => Profit::whereDate('created_at','>=',$this->$start_date)->whereDate('created_at','<=',$this->end_date)->sum('total')
+            'total' => Profit::whereDate('created_at','>=', $this->start_date)->whereDate('created_at','<=',$this->end_date)->sum('total')
         ]);
     }
 }
